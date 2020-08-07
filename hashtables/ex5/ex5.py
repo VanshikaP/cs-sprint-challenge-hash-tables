7b51d9f1199a6845cc2924fc0314a124c75f1116
VanshikaP <vanshikapundir92@gmail.com>
2020-08-07 08:58:03 -0700

    ex-5 - stretch

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,6 +7,25 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+    result = []
+
+    # map file names to their path
+    filePathMap = {}
+    for path in files:
+        address = path.split('/')
+        filename = address[len(address) - 1]
+
+        if filename in filePathMap:
+            filePathMap[filename].append(path)
+        else:
+            filePathMap[filename] = [path]
+    
+    # get paths for filenames in queries
+    for filename in queries:
+        if filename in filePathMap:
+            for path in filePathMap[filename]:
+                result.append(path)
+
 
     return result
 
